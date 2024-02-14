252ee4112c1ef06c032d19ffe65bc9fa15d8b9e0
guillermosequera <sequeracrespo@gmail.com>
2024-02-14 01:20:42 -0300

    creando nuevo todo

--- 05-todo-app/src/todos/models/todo.model.js
+++ 05-todo-app/src/todos/models/todo.model.js
@@ -4,7 +4,7 @@ import { v4 as uuid } from "uuid";
 export class Todo {
 
     constructor( description ){
-        this.id = uuid();
+        this.id = uuid;
         this.description = description;
         this.done = false;
         this.createdAt = new Date();
